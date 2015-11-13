e8952f36d4a6ae3b5a12b2c4feb80e9953d9ff7a
I-am-Gabi <gabicavalcantesilva@gmail.com>
2015-11-13 01:20:59 +0100

    fix the class test
    change the old class test (Greeter Test) to new class (Explorer), resolved #QAB-7

--- src/test/java/ExplorerTest.java
+++ src/test/java/ExplorerTest.java
@@ -5,28 +5,10 @@ import static org.junit.Assert.*;
 
 public class ExplorerTest {
 
-	Greeter greeter;
+	Explorer e;
 
 	@Before
 	public void defineContext() {
-		greeter = new Greeter();
+		e = new Explorer();
 	}
-
-	@Test
-	public void sayHelloToSomeone() {
-		String seb = greeter.sayHello("Sebastien");
-		assertEquals("Hello, Sebastien!", seb);
-	}
-
-	@Test
-	public void sayHelloToTheWorld() {
-		String world = greeter.sayHello();
-		assertEquals("Hello, World!", world);
-	}
-
-	@Test
-	public void sayHelloEquivalence() {
-		assertEquals(greeter.sayHello(), greeter.sayHello("World"));
-	}
-
 }
