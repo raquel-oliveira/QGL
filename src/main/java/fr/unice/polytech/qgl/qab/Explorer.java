fc1462177cbe4390f38494a109e65d86c2b1f973
I-am-Gabi <gabicavalcantesilva@gmail.com>
2015-11-14 18:57:25 +0100

    Create a Minimal Viable Product - implementing the class explorer to the minimal bot #QAB-9

--- src/main/java/fr/unice/polytech/qgl/qab/Explorer.java
+++ src/main/java/fr/unice/polytech/qgl/qab/Explorer.java
@@ -1,23 +1,37 @@
 package fr.unice.polytech.qgl.qab;
 
-import org.json.JSONObject;
-
+/**
+ * Class that represents the bot in the game.
+ * Description based on documentation (http://ace-design.github.io/island/bot/)
+ *
+ * @version 13.11.2015
+ */
 public class Explorer implements IExplorerRaid{
 
-    public Explorer() {
-    }
+    public Explorer() {}
 
+    /**
+     * Method to initiate the game. It is invoked right after the initialization.
+     * @param context assignment (modeled as a JSON data structure) with the main information to initiate the game.
+     */
     public void initialize(String context) {
-        throw new UnsupportedOperationException("IExplorerRaid::initialize is not yet implemented");
+        return;
     }
 
-
+    /**
+     * Method responsible for take decisions, invoked each time the bot must decide which action must be played.
+     * @return for now, we always return the same action: stopping the game
+     */
     public String takeDecision() {
-        throw new UnsupportedOperationException("IExplorerRaid::takeDecision is not yet implemented");
+        return "{ \"action\": \"stop\" }";
     }
 
-
+    /**
+     * The acknowledgeResults(String) method is invoked right after the takeDecision() method.
+     * It provides the results of the action when applied to the world.
+     * @param results
+     */
     public void acknowledgeResults(String results) {
-        throw new UnsupportedOperationException("IExplorerRaid::acknowledgeResults is not yet implemented");
+        return;
     }
 }
