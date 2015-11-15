8113b847baf5c95502d8e4711f69eb560fe3dd44
Quentin PROD'HOMME <pq100481@etu.unice.fr>
2015-11-15 02:11:55 +0100

    #QAB-12 minimal test, will be updated when the logic behind the bot will be more complex

--- src/test/java/fr/unice/polytech/qgl/qab/ExplorerTest.java
+++ src/test/java/fr/unice/polytech/qgl/qab/ExplorerTest.java
@@ -2,9 +2,8 @@ package fr.unice.polytech.qgl.qab;
 
 import org.junit.Before;
 import org.junit.Test;
-import fr.unice.polytech.qgl.qab.Explorer;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class ExplorerTest {
 
@@ -13,5 +12,12 @@ public class ExplorerTest {
 	@Before
 	public void defineContext() {
 		e = new Explorer();
+	}
+
+	// Basic test, will be updated when the logic behind our bot will develop
+	@Test
+	public void simpleTakeDecision() {
+		String s = e.takeDecision();
+		assertTrue(s.equals("{ \"action\": \"stop\" }"));
 	}
 }
