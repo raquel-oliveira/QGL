ecaac78a87ea6b983a3a571d94cb908220242b5e
I-am-Gabi <gabicavalcantesilva@gmail.com>
2015-11-21 16:40:22 +0100

    adding reading of the context (json) for initialization #QAB-18

--- src/main/java/fr/unice/polytech/qgl/qab/Explorer.java
+++ src/main/java/fr/unice/polytech/qgl/qab/Explorer.java
@@ -1,5 +1,10 @@
 package fr.unice.polytech.qgl.qab;
 
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.util.HashMap;
+
 import eu.ace_design.island.bot.IExplorerRaid;
 /**
  * Class that represents the bot in the game.
@@ -8,15 +13,43 @@ import eu.ace_design.island.bot.IExplorerRaid;
  * @version 13.11.2015
  */
 public class Explorer implements IExplorerRaid{
+    private int men, budget;
+    private String heading;
+    private HashMap<String, Integer> contracts;
+    private String takeAction;
 
-    public Explorer() {}
+    public Explorer() {
+        men = 0;
+        budget = 0;
+        heading = "";
+        contracts = new HashMap<String, Integer>();
+        takeAction = "{ \"action\": \"fly\" }";
+    }
 
     /**
      * Method to initiate the game. It is invoked right after the initialization.
      * @param context assignment (modeled as a JSON data structure) with the main information to initiate the game.
      */
     public void initialize(String context) {
-        return;
+        JSONObject jsonObj = new JSONObject(context);
+
+        men = jsonObj.getInt("men");
+        budget = jsonObj.getInt("budget");
+
+        JSONArray cont = jsonObj.getJSONArray("contracts");
+
+        for (int i = 0; i < cont.length();  i++) {
+            String key = cont.getJSONObject(i).getString("resource");
+            int value = cont.getJSONObject(i).getInt("amount");
+            contracts.put(key, value);
+        }
+
+        heading = (String)jsonObj.get("heading");
+
+        System.out.println("men: " + men);
+        System.out.println("budget: " + budget);
+        System.out.println("contracts: " + contracts);
+        System.out.println("heading: " + heading);
     }
 
     /**
@@ -24,7 +57,7 @@ public class Explorer implements IExplorerRaid{
      * @return for now, we always return the same action: stopping the game
      */
     public String takeDecision() {
-        return "{ \"action\": \"stop\" }";
+        return takeAction;
     }
 
     /**
@@ -33,6 +66,20 @@ public class Explorer implements IExplorerRaid{
      * @param results information returned after as result of the engine action
      */
     public void acknowledgeResults(String results) {
-        return;
+        JSONObject jsonObj = new JSONObject(results);
+        int cost = jsonObj.getInt("cost");
+        String status = jsonObj.getString("status");
+        if (status.compareToIgnoreCase("ok") != 0)
+            return;
+
+        budget = budget - cost;
+
+        if (budget > 15) {
+            if (takeAction.contains("fly") == true)
+                takeAction = "{ \"action\": \"echo\", \"parameters\": { \"direction\": \"" + heading + "\" } }";
+            else takeAction = "{ \"action\": \"fly\" }";
+        }
+        else
+            takeAction = "stop";
     }
 }
