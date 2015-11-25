76ef06f141f585ef65c6903fcb9ec245634cca41
I-am-Gabi <gabicavalcantesilva@gmail.com>
2015-11-25 10:46:54 +0100

    Restructure explorer class, created the class ActionPlane to action of first phase #QAB-42

--- src/main/java/fr/unice/polytech/qgl/qab/Explorer.java
+++ src/main/java/fr/unice/polytech/qgl/qab/Explorer.java
@@ -4,45 +4,30 @@ import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Random;
+
 /**
  * Class that represents the bot in the game.
  * Description based on documentation (http://ace-design.github.io/island/bot/)
  *
- * @version 21.11.2015
+ * @version 4.8
  */
 public class Explorer implements IExplorerRaid{
     private int men, budget;
-    private String heading;
-    //private Direction heading;
-    //private Direction direction;
+    private Direction heading;
     private HashMap<String, Integer> contracts;
     private String takeAction;
-    private boolean foundOut;
-    private int rangeOut;
-    private boolean foundGroud;
-    private int rangeGroud;
-    private String directionFound;
-    private ArrayList<String> biomes;
-    private ArrayList<String> creeks;
-    private String currentAction;
+    private ActionPlane plane;
+    private int dir;
 
     public Explorer() {
         men = 0;
         budget = 0;
-        heading = "";
-        //heading = Direction.EAST;
+        heading = null;
         contracts = new HashMap<String, Integer>();
         takeAction = null;
-        foundOut = false;
-        rangeOut = -1;
-        directionFound = null;
-        foundGroud = false;
-        rangeGroud = -1;
-        biomes = new ArrayList<String>();
-        creeks = new ArrayList<String>();
+        plane = new ActionPlane();
+        dir = 0;
     }
 
     /**
@@ -63,8 +48,7 @@ public class Explorer implements IExplorerRaid{
             contracts.put(key, value);
         }
 
-        heading = (String)jsonObj.get("heading");
-        //heading = (Direction.valueOf((String) jsonObj.getString("handing")));
+       heading = Direction.fromString(jsonObj.getString("heading"));
     }
 
     /**
@@ -72,69 +56,51 @@ public class Explorer implements IExplorerRaid{
      * @return for now, we always return the same action: stopping the game
      */
     public String takeDecision() {
-        Random gerador = new Random();
-        int direction = 0;
-
-        // if in the range": 0, "found": "OUT_OF_RANGE". Stop doing everything.
-        if (rangeOut == 0 && foundOut){
+        if ((plane.rangeOutOfRange(heading.toString()) <= 1 && plane.rangeOutOfRange(heading.toString()) >= 0) || budget <= 100){
             takeAction = "STOP";
             return "{ \"action\": \"stop\" }";
         }
-        else if (budget <= 100 ) {
-            takeAction = "STOP";
-            return "{ \"action\": \"stop\" }";
-        }
-        else if ((takeAction == null || takeAction.compareToIgnoreCase("scan") == 0) && !foundOut && rangeGroud <= 0) {
-            directionFound = heading;
+        else if (takeAction == null || plane.makeEcho(takeAction, heading.toString())) {
             takeAction = "ECHO";
-            return "{ \"action\": \"echo\", \"parameters\": { \"direction\": \"" + heading + "\" } }";
+            return "{ \"action\": \"echo\", \"parameters\": { \"direction\": \""+ heading.toString().toUpperCase() +"\" } }";
         }
-        else if (takeAction.compareToIgnoreCase("fly") == 0 || takeAction.compareToIgnoreCase("heading") == 0){
+        /*else if (plane.makeScan(heading.toString(), takeAction)){
         	takeAction = "SCAN";
         	return "{ \"action\": \"scan\" }";
-        }
-/*        else if (foundOut && rangeOut ==  1) { // if the plane found the out_range
-            if (heading.compareToIgnoreCase(directionFound) != 0) { // if the plane made the echo before
-                if ((foundOut && (rangeOut > 1)) || foundGroud) { // if the plane found that is ok to change
-                    foundGroud = foundOut = false;
-                    rangeGroud =  rangeOut = -1;
-                    takeAction = "HEADING";
-                    return "{ \"action\": \"heading\", \"parameters\": { \"direction\": \"" + directionFound + "\" } }";
-                }
-            }
-            if (heading.compareToIgnoreCase("N") == 0 || heading.compareToIgnoreCase("S") == 0) {
-                direction = gerador.nextInt(2);
-                directionFound = (direction == 1)?"W":"E";
-                heading = directionFound; // change the direction of heading
-                takeAction = "HEADING";
-                return "{ \"action\": \"echo\", \"parameters\": { \"direction\": \"" + directionFound + "\" } }";
-            }
-            else if (heading.compareToIgnoreCase("E") == 0 || heading.compareToIgnoreCase("W") == 0){
-                direction = gerador.nextInt(2);
-                directionFound = (direction == 1)?"N":"S";
-                heading = directionFound; // change the direction of heading
-                takeAction = "HEADING";
-                return "{ \"action\": \"echo\", \"parameters\": { \"direction\": \"" + directionFound + "\" } }";
-            }
-<<<<<<< HEAD
-        }
-        else if((takeAction.compareToIgnoreCase("FLY") == 0) || (takeAction.compareToIgnoreCase("HEADING") == 0)) {
-            takeAction = "HEADING";
-            return "{ \"action\": \"scan\" }";
-        }
-=======
         }*/
->>>>>>> d0182ac39e4fe35383f052a461d340d2bb54766a
-        else {
-            takeAction = "FLY";
-            if (foundOut)
-                rangeOut--;
-            if (foundGroud)
-                rangeGroud--;
-
-            return "{ \"action\": \"fly\" }";
+        else if (plane.makeHeading(heading.toString(), takeAction)) {
+            takeAction = "HEADING";
+            plane.resetEnvironment();
+            if (dir == 0) {
+                heading = Direction.fromString("s");
+                dir = 1;
+                return "{ \"action\": \"heading\", \"parameters\": { \"direction\": \"S\" } }";
+            } else if (dir == 1) {
+                heading = Direction.fromString("e");
+                dir = 0;
+                return "{ \"action\": \"heading\", \"parameters\": { \"direction\": \"E\" } }";
+            }
         }
-//        return null;
+        /*else if (plane.makeHeading(heading.toString())) {
+            String resul = plane.directionToHeading(heading);
+            if (resul.compareToIgnoreCase("ECHO") == 0) {
+                resul = plane.whereEcho(heading);
+                if (resul.compareToIgnoreCase("FLY") == 0) {
+                    takeAction = "FLY";
+                    plane.fly(heading.toString());
+                    return "{ \"action\": \"fly\" }";
+                }
+                takeAction = "ECHO";
+                return "{ \"action\": \"echo\", \"parameters\": { \"direction\": \""+ resul.toUpperCase() +"\" } }";
+            }
+            takeAction = "HEADING";
+            plane.resetEnvironment();
+            heading = Direction.fromString(resul);
+            return "{ \"action\": \"heading\", \"parameters\": { \"direction\": \""+ resul.toUpperCase() +"\" } }";
+        }*/
+        takeAction = "FLY";
+        plane.fly(heading.toString());
+        return "{ \"action\": \"fly\" }";
     }
 
     /**
@@ -151,52 +117,21 @@ public class Explorer implements IExplorerRaid{
             takeAction = "stop";
 
         budget = budget - cost;
+
         if (takeAction.compareToIgnoreCase("ECHO") == 0) {
-            int range = 0;
-            String found = "";
-            if (jsonObj.getJSONObject("extras").has("range"))
-                range = jsonObj.getJSONObject("extras").getInt("range");
+            String found = null;
+            Integer range = null;
 
             if (jsonObj.getJSONObject("extras").has("found"))
                 found = jsonObj.getJSONObject("extras").getString("found");
+            if (jsonObj.getJSONObject("extras").has("range"))
+                range = jsonObj.getJSONObject("extras").getInt("range");
 
-            if (found.compareToIgnoreCase("GROUND") == 0) {
-                foundGroud = true;
-                rangeGroud = range;
-            } else if (found.compareToIgnoreCase("OUT_OF_RANGE") == 0) {
-                foundOut = true;
-                rangeOut = range;
-            }
+            if (found.compareToIgnoreCase("GROUND") == 0)
+                plane.setGround(heading.toString(), Integer.toString(range));
+            else
+                plane.setOutOfRange(heading.toString(), Integer.toString(range));
         }
-//        else if (takeAction.compareToIgnoreCase("SCAN") == 0) {
-//            String bios = null;
-//            String cks = null;
-//
-//            if (jsonObj.getJSONObject("extras").has("biomes"))
-//                bios = jsonObj.getJSONObject("extras").getString("biomes");
-//
-//            if (jsonObj.getJSONObject("extras").has("creeks"))
-//                bios = jsonObj.getJSONObject("extras").getString("creeds");
-//
-//            if(bios != null && !bios.isEmpty()) {
-//                String[] listBios = bios.split(",");
-//                for (String b : listBios)
-//                    biomes.add(b);
-//            }
-//            if(cks != null && !cks.isEmpty()) {
-//                String[] listCks = cks.split(",");
-//                for (String c : listCks)
-//                    creeks.add(c);
-//            }
-//        }
     }
-    public int getRangeOut(){
-        return rangeOut;
-    }
-    public boolean isFoundOut(){
-        return foundOut;
-    }
-    public String getTakeAction(){
-        return takeAction;
-    }
+
 }
