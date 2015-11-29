72b545cb0fdcc0a03f78579f24e528325f7d0792
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-11-30 00:22:39 +0100

    adding comments to the methods

--- src/main/java/fr/unice/polytech/qgl/qab/Direction.java
+++ src/main/java/fr/unice/polytech/qgl/qab/Direction.java
@@ -6,27 +6,50 @@ package fr.unice.polytech.qgl.qab;
  * @version 4.8
  */
 public enum Direction {
-    NORTH("n"),
-    EAST("e"),
-    SOUTH("s"),
-    WEST("w");
-
-    public Boolean isHorizontal() {
-        return this == EAST || this == WEST;
-    }
-
-    public Boolean isVertical() {
-        return this == NORTH || this == SOUTH;
-    }
+    NORTH("N"),
+    EAST("E"),
+    SOUTH("S"),
+    WEST("W");
 
     private String name = "";
 
+    /**
+     * Constructor enum
+     * @param name
+     */
     Direction(String name) {
         this.name = name;
     }
 
-    public String toString() { return name; }
+    /**
+     * Method that return if the direction is horizontal
+     * @return true if the direction if horizontal, false if not
+     */
+    public Boolean isHorizontal() {
+        return this == EAST || this == WEST;
+    }
 
+    /**
+     * Method that return if the direction if vertical
+     * @return true if the direction if vertical, false if not
+     */
+    public Boolean isVertical() {
+        return this == NORTH || this == SOUTH;
+    }
+
+    /**
+     * Method that change the direction to String
+     * @return name
+     */
+    public String toString() {
+        return name;
+    }
+
+    /**
+     * Method that receive a direction like string and return a object Direction
+     * @param direction string with the direction
+     * @return object Direction
+     */
     public static Direction fromString(String direction){
         if (direction != null){
             for (Direction d : Direction.values()){
