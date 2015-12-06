02b7d533116c070d6e97ab8466830f90283f9ccc
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-06 17:06:44 +0100

    test to tile #QAB-66

---
+++ src/test/java/fr/unice/polytech/qgl/qab/tileTest/TileTest.java
@@ -0,0 +1,66 @@
+package fr.unice.polytech.qgl.qab.tileTest;
+
+import fr.unice.polytech.qgl.qab.tile.Tile;
+import fr.unice.polytech.qgl.qab.tile.TileType;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Gabriela
+ * @version 4.9
+ */
+public class TileTest {
+    Tile t;
+
+    @Test
+    public void testInitializaTitle() {
+        t = new Tile();
+        assertEquals(TileType.UNDEFINED, t.getType());
+    }
+
+    @Test
+    public void testInitializaTitleWithTypeGround() {
+        t = new Tile(TileType.GROUND);
+        assertEquals(TileType.GROUND, t.getType());
+    }
+
+    @Test
+    public void testInitializaTitleWithTypeOcean() {
+        t = new Tile(TileType.OCEAN);
+        assertEquals(TileType.OCEAN, t.getType());
+    }
+
+    @Test
+    public void testChangeTypeOfTileToGround() {
+        t = new Tile();
+        assertEquals(TileType.UNDEFINED, t.getType());
+
+        t.setType(TileType.GROUND);
+        assertEquals(TileType.GROUND, t.getType());
+    }
+
+    @Test
+    public void testChangeTypeOfTileToOcean() {
+        t = new Tile();
+        assertEquals(TileType.UNDEFINED, t.getType());
+
+        t.setType(TileType.OCEAN);
+        assertEquals(TileType.OCEAN, t.getType());
+    }
+
+    @Test
+    public void testChangeTypeOfTileToUndefined() {
+        t = new Tile(TileType.OCEAN);
+        assertEquals(TileType.OCEAN, t.getType());
+
+        t.setType(TileType.UNDEFINED);
+        assertEquals(TileType.UNDEFINED, t.getType());
+
+        t = new Tile(TileType.GROUND);
+        assertEquals(TileType.GROUND, t.getType());
+
+        t.setType(TileType.UNDEFINED);
+        assertEquals(TileType.UNDEFINED, t.getType());
+    }
+}
---
+++ src/test/java/fr/unice/polytech/qgl/qab/tileTest/TileTypeTest.java
@@ -0,0 +1,89 @@
+package fr.unice.polytech.qgl.qab.tileTest;
+
+import fr.unice.polytech.qgl.qab.tile.TileType;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * @author Gabriela
+ * @version 4.9
+ */
+public class TileTypeTest {
+
+    TileType type;
+
+    @Test
+    public void testTileTypeGround() {
+        type = TileType.GROUND;
+        assertEquals(TileType.GROUND, type);
+    }
+
+    @Test
+    public void testTileTypeOcean() {
+        type = TileType.OCEAN;
+        assertEquals(TileType.OCEAN, type);
+    }
+
+    @Test
+    public void testTileTypeUndefined() {
+        type = TileType.UNDEFINED;
+        assertEquals(TileType.UNDEFINED, type);
+    }
+
+    @Test
+    public void testToStringUndefined() {
+        type = TileType.UNDEFINED;
+        assertEquals(TileType.UNDEFINED.toString(), type.toString());
+    }
+
+    @Test
+    public void testToStringGround() {
+        type = TileType.GROUND;
+        assertEquals(TileType.GROUND.toString(), type.toString());
+    }
+
+    @Test
+    public void testToStringOcean() {
+        type = TileType.OCEAN;
+        assertEquals(TileType.OCEAN.toString(), type.toString());
+    }
+
+    @Test
+    public void testFromStringOcean() {
+        type = TileType.fromString("ocean");
+        assertEquals(TileType.OCEAN, type);
+    }
+
+    @Test
+    public void testFromStringGround() {
+        type = TileType.fromString("ground");
+        assertEquals(TileType.GROUND, type);
+    }
+
+    @Test
+    public void testFromStringUndefined() {
+        type = TileType.fromString("undefined");
+        assertEquals(TileType.UNDEFINED, type);
+    }
+
+    @Test
+    public void testFromStringNotExists() {
+        type = TileType.fromString("error");
+        assertEquals(null, type);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testToStringBeforeInitialize() {
+        type.toString();
+    }
+
+    @Test
+    public void testToStringBeforeInitialize2() {
+        try {
+            type.toString();
+            fail();
+        } catch (NullPointerException e) {}
+    }
+}
