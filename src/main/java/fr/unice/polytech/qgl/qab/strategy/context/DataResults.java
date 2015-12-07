b3e229d6d79781b6bae7d7fb4d843ba41d98dcd1
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-07 04:27:39 +0100

    Fixing the dataResults to read the data and change the map size #QAB-64

--- src/main/java/fr/unice/polytech/qgl/qab/strategy/context/DataResults.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/context/DataResults.java
@@ -21,36 +21,10 @@ public class DataResults {
         contextIsland.setStatus((jsonObj.getString("status").compareToIgnoreCase("ok") == 0)? true:false);
         contextIsland.setBudget(contextIsland.getBudget() - jsonObj.getInt("cost"));
 
-        if(takeAction.equals(ActionBot.ECHO))
+        if(takeAction instanceof Echo)
             readEcho(contextIsland, takeAction, jsonObj);
-        else if (takeAction.equals(ActionBot.SCAN)) {
-            readScan(jsonObj);
-        }
+
         return contextIsland;
-    }
-
-    private static void readScan(JSONObject jsonObj) {
-        ArrayList<String> biomes = new ArrayList<String>();
-        ArrayList<String> creeks = new ArrayList<String>();
-        String bio = null;
-        String crk = null;
-
-        if(jsonObj.getJSONObject("extras").has("biomes"))
-            bio = jsonObj.getJSONObject("extras").getString("biomes");
-        if(jsonObj.getJSONObject("extras").has("creeks"))
-            crk = jsonObj.getJSONObject("extras").getString("creeks");
-
-        if(bio != null && !bio.isEmpty()){
-            String[] biomelist = bio.split(",");
-            for(String b : biomelist)
-                biomes.add(b);
-        }
-
-        if(crk != null && !crk.isEmpty()){
-            String[] creeklist = crk.split(",");
-            for(String c : creeklist)
-                creeks.add(c);
-        }
     }
 
     private static void readEcho(Context context, Action takeAction, JSONObject jsonObj) {
@@ -65,17 +39,18 @@ public class DataResults {
     }
 
     private static void initializaSize(Context context, Echo takeAction, Found found, Integer range) {
-        if (found.compareTo(Found.OUT_OF_RANGE) == 0 &&
-                context.getHeading().compareTo(takeAction.getDirection()) == 0) {
-            if (context.getHeading().isVertical()) {
+        if (found.equals(Found.OUT_OF_RANGE)) {
+            if (takeAction.getDirection().isVertical()) {
                 context.setHeight(range + 1);
             } else {
                 context.setWidth(range + 1);
             }
         } else {
-            if (context.getHeading().isVertical())
-                context.setWidth(context.getWidth() + range + 1);
-            else context.setHeight(context.getHeight() + range + 1);
+            if (takeAction.getDirection().isVertical()) {
+                context.setWidth(range + 1);
+            } else {
+                context.setHeight(range + 1);
+            }
         }
     }
 }
