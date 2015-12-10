9c352886361beead8dbf634631157885aeb5b2f4
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-10 21:35:30 +0100

    add get e set in direction #QAB-71

--- src/main/java/fr/unice/polytech/qgl/qab/util/Discovery.java
+++ src/main/java/fr/unice/polytech/qgl/qab/util/Discovery.java
@@ -1,5 +1,6 @@
 package fr.unice.polytech.qgl.qab.util;
 
+import fr.unice.polytech.qgl.qab.util.enums.Direction;
 import fr.unice.polytech.qgl.qab.util.enums.Found;
 
 /**
@@ -8,10 +9,23 @@ import fr.unice.polytech.qgl.qab.util.enums.Found;
 public class Discovery {
     private Found found;
     private int range;
+    private Direction direction;
+
+    public Discovery() {
+        this.found = Found.UNDEFINED;
+        this.range = 0;
+        this.direction = null;
+    }
 
     public Discovery(Found found, int range) {
         this.found = found;
         this.range = range;
+    }
+
+    public Discovery(Found found, int range, Direction direction) {
+        this.found = found;
+        this.range = range;
+        this.direction = direction;
     }
 
     public Found getFound() {
@@ -22,7 +36,20 @@ public class Discovery {
         return range;
     }
 
+    public Direction getDirection() {
+        return direction;
+    }
+
     public void setRange(int range) {
         this.range = range;
     }
+
+    public void setFound(Found found) {
+        this.found = found;
+    }
+
+    public void setDirection(Direction direction) {
+        this.direction = direction;
+    }
+
 }
