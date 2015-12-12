6e5f4cd48311f16a6507c1afa1f615be765a4203
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2015-12-12 22:23:43 +0100

    Change of definitions of state (Gabriela)

--- src/main/java/fr/unice/polytech/qgl/qab/strategy/aerial/States/State1.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/aerial/States/State1.java
@@ -2,25 +2,29 @@ package fr.unice.polytech.qgl.qab.strategy.aerial.States;
 
 import fr.unice.polytech.qgl.qab.actions.Action;
 import fr.unice.polytech.qgl.qab.actions.aerial.Echo;
+import fr.unice.polytech.qgl.qab.actions.aerial.Fly;
 import fr.unice.polytech.qgl.qab.actions.aerial.Heading;
 import fr.unice.polytech.qgl.qab.actions.aerial.combo.ComboFlyEcho;
-import fr.unice.polytech.qgl.qab.actions.common.Stop;
+import fr.unice.polytech.qgl.qab.map.Map;
 import fr.unice.polytech.qgl.qab.strategy.context.Context;
 import fr.unice.polytech.qgl.qab.strategy.context.ResponseState;
+import fr.unice.polytech.qgl.qab.strategy.context.UpdaterMap;
 import fr.unice.polytech.qgl.qab.util.enums.Found;
 
 /**
  * @version 11.12.2015.
  */
-public class State1 implements IState {
+public class State1 extends State {
     public static State1 instance;
 
     private ComboFlyEcho actionCombo;
-    private ResponseState response;
+    private Action lastAction;
+    private UpdaterMap updaterMap;
 
     protected State1() {
-        actionCombo = new ComboFlyEcho();
-        response = null;
+        actionCombo = null;
+        lastAction = new Fly();
+        updaterMap = new UpdaterMap();
     }
 
     public static State1 getInstance() {
@@ -30,21 +34,45 @@ public class State1 implements IState {
     }
 
     @Override
-    public ResponseState responseState(Context context) {
-        if (context.getLastDiscovery().getFound().equals(Found.GROUND)) {
-            response.setAction(new Heading(((Echo)response.getAction()).getDirection()));
-            response.setStatus(true);
-            return response;
+    public State getState(Context context, Map map) {
+        if (actionCombo != null && actionCombo.isEmpty())
+            updateContext(context, map);
+
+        if (lastAction instanceof Heading)
+            return State2.getInstance();
+
+        return State1.getInstance();
+    }
+
+    @Override
+    public Action responseState(Context context, Map map) {
+        Action act = null;
+        if (context.getLastDiscovery().getFound().equals(Found.GROUND) && lastAction instanceof Echo) {
+            act = new Heading(((Echo)lastAction).getDirection());
+            lastAction = act;
+            return act;
         }
 
-        if (actionCombo.isEmpty())
+        if (actionCombo == null || actionCombo.isEmpty()) {
+            actionCombo = new ComboFlyEcho();
             actionCombo.defineActions(context.getHeading());
+        }
 
-        Action act = actionCombo.get(0);
+        act = actionCombo.get(0);
+
+        if (act instanceof Echo) {
+            lastAction = (Echo) act;
+        } else if (act instanceof Fly) {
+            lastAction = (Fly) act;
+            updateContext(context, map);
+        }
+
         actionCombo.remove(0);
 
-        response = new ResponseState(act, false);
+        return act;
+    }
 
-        return response;
+    private void updateContext(Context context, Map map) {
+        updaterMap.updateLastPositionFly(context, map);
     }
 }
