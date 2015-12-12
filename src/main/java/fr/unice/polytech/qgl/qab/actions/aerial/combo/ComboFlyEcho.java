5870c5361f88f149fd60aa695cf274ac10980d81
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-12 02:33:15 +0100

    add actions combos and state1

---
+++ src/main/java/fr/unice/polytech/qgl/qab/actions/aerial/combo/ComboFlyEcho.java
@@ -0,0 +1,70 @@
+package fr.unice.polytech.qgl.qab.actions.aerial.combo;
+
+import fr.unice.polytech.qgl.qab.actions.Action;
+import fr.unice.polytech.qgl.qab.actions.aerial.Echo;
+import fr.unice.polytech.qgl.qab.actions.aerial.Fly;
+import fr.unice.polytech.qgl.qab.util.enums.Direction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @version 11.12.2015
+ */
+public class ComboFlyEcho {
+    private List<Action> actions;
+
+    public ComboFlyEcho() {
+        actions = new ArrayList<>();
+    }
+
+    /**
+     * Set the actions (fly and 2 echos
+     * @param head direction of the head
+     */
+    public void defineActions(Direction head) {
+        actions.add(new Fly());
+        if (head.isHorizontal()) {
+            actions.add(new Echo(Direction.NORTH));
+            actions.add(new Echo(Direction.SOUTH));
+        }
+        if (head.isVertical()) {
+            actions.add(new Echo(Direction.WEST));
+            actions.add(new Echo(Direction.EAST));
+        }
+    }
+
+    /**
+     * Method to return the list of the actions (echos).
+     * @return actions list
+     */
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    /**
+     * Method that check if the action list is empty
+     * @return true if the list is empty, false if not
+     */
+    public boolean isEmpty() {
+        return actions.isEmpty();
+    }
+
+    /**
+     * Return one action in a defined index.
+     * @param index of the action to return
+     * @return action chosen
+     */
+    public Action get(int index) {
+        return actions.get(index);
+    }
+
+    /**
+     * Method to remove a item that is in the index gave.
+     * @param index to remove item
+     * @return the item removed
+     */
+    public Action remove(int index) {
+        return actions.remove(index);
+    }
+}
--- src/main/java/fr/unice/polytech/qgl/qab/strategy/aerial/States/State1.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/aerial/States/State1.java
@@ -1,7 +1,50 @@
 package fr.unice.polytech.qgl.qab.strategy.aerial.States;
 
+import fr.unice.polytech.qgl.qab.actions.Action;
+import fr.unice.polytech.qgl.qab.actions.aerial.Echo;
+import fr.unice.polytech.qgl.qab.actions.aerial.Heading;
+import fr.unice.polytech.qgl.qab.actions.aerial.combo.ComboFlyEcho;
+import fr.unice.polytech.qgl.qab.actions.common.Stop;
+import fr.unice.polytech.qgl.qab.strategy.context.Context;
+import fr.unice.polytech.qgl.qab.strategy.context.ResponseState;
+import fr.unice.polytech.qgl.qab.util.enums.Found;
+
 /**
- * Created by gabriela on 11/12/15.
+ * @version 11.12.2015.
  */
-public class State1 {
+public class State1 implements IState {
+    public static State1 instance;
+
+    private ComboFlyEcho actionCombo;
+    private ResponseState response;
+
+    protected State1() {
+        actionCombo = new ComboFlyEcho();
+        response = null;
+    }
+
+    public static State1 getInstance() {
+        if (instance == null)
+            instance = new State1();
+        return instance;
+    }
+
+    @Override
+    public ResponseState responseState(Context context) {
+        if (context.getLastDiscovery().getFound().equals(Found.GROUND)) {
+            response.setAction(new Heading(((Echo)response.getAction()).getDirection()));
+            response.setStatus(true);
+            return response;
+        }
+
+        if (actionCombo.isEmpty())
+            actionCombo.defineActions(context.getHeading());
+
+        Action act = actionCombo.get(0);
+        actionCombo.remove(0);
+
+        response = new ResponseState(act, false);
+
+        return response;
+    }
 }
