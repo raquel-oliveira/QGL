06312a4d3af45430025a8255623657e0cb5c01d9
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2015-12-13 16:59:22 +0100

    Definition of biomes #QAB-28

--- src/main/java/fr/unice/polytech/qgl/qab/util/enums/Biomes.java
+++ src/main/java/fr/unice/polytech/qgl/qab/util/enums/Biomes.java
@@ -6,25 +6,25 @@ package fr.unice.polytech.qgl.qab.util.enums;
 public enum Biomes {
     OCEAN("OCEAN"), //: plain ocean, wide open area full of unknown;
     LAKE("LAKE"), //: internal lake, potentially big, with freshwater;
-    BEACH(""), //: beach (not always sandy);
-    GRASSLAND(""),//: area of prairie;
+    BEACH("BEACH"), //: beach (not always sandy);
+    GRASSLAND("GRASSLAND"),//: area of prairie;
 
-    MANGROVE(""),//: super wet area, home of the mangle tree;
-    TROPICAL_RAIN_FOREST(""), //: hot and wet;
-    TROPICAL_SEASONAL_FOREST(""),//: less wet, but not less hot;
+    MANGROVE("MANGROVE"),//: super wet area, home of the mangle tree;
+    TROPICAL_RAIN_FOREST("TROPICAL_RAIN_FOREST"), //: hot and wet;
+    TROPICAL_SEASONAL_FOREST("TROPICAL_SEASONAL_FOREST"),//: less wet, but not less hot;
 
-    TEMPERATE_DECIDUOUS_FOREST(""), //: classical forests with trees that lose their leaves each year;
-    TEMPERATE_RAIN_FOREST(""),//: very rare biome, very wet area coupled to low temperatures;
-    TEMPERATE_DESERT(""),//: aride area with sparse vegetation and very few humidity;
+    TEMPERATE_DECIDUOUS_FOREST("TEMPERATE_DECIDUOUS_FOREST"), //: classical forests with trees that lose their leaves each year;
+    TEMPERATE_RAIN_FOREST("TEMPERATE_RAIN_FOREST"),//: very rare biome, very wet area coupled to low temperatures;
+    TEMPERATE_DESERT("TEMPERATE_DESERT"),//: aride area with sparse vegetation and very few humidity;
 
-    TAIGA(""),//: boreal forest, cold and majestuous;
-    SNOW(""),//: area covered with snow, wet and cold;
-    TUNDRA(""),//: arctic prairie, surviving on permanently frozen soil;
-    ALPINE(""),//: rocky mountains, not always covered by snow;
-    GLACIER(""),//: inhospitable area, full of ice;
+    TAIGA("TAIGA"),//: boreal forest, cold and majestuous;
+    SNOW("SNOW"),//: area covered with snow, wet and cold;
+    TUNDRA("TUNDRA"),//: arctic prairie, surviving on permanently frozen soil;
+    ALPINE("ALPINE"),//: rocky mountains, not always covered by snow;
+    GLACIER("GLACIER"),//: inhospitable area, full of ice;
 
-    SHRUBLAND(""),//: prairie dominated by shrubs, such as maquis in Corsica or garrigue in Provence;
-    SUB_TROPICAL_DESERT("");// very dry and inhospitable area
+    SHRUBLAND("SHRUBLAND"),//: prairie dominated by shrubs, such as maquis in Corsica or garrigue in Provence;
+    SUB_TROPICAL_DESERT("SUB_TROPICAL_DESERT");// very dry and inhospitable area
 
     private String name = "";
 
@@ -64,11 +64,12 @@ public enum Biomes {
 
     public TypeBiome typeBiome(Biomes biome){
         if (biome != null){
-            if (isNordic()) return TypeBiome.NORDIC;
-            if (isTropical()) return  TypeBiome.TROPICAL;
-            if (isSubTropical()) return TypeBiome.SUBTROPICAL;
-            if (isTemperate()) return TypeBiome.TEMPERATE;
-            return TypeBiome.COMMON;
+            if (isCommon()) return TypeBiome.COMMON;
+            else if (isNordic()) return TypeBiome.NORDIC;
+            else if (isTropical()) return  TypeBiome.TROPICAL;
+            else if (isSubTropical()) return TypeBiome.SUBTROPICAL;
+            else if (isTemperate()) return TypeBiome.TEMPERATE;
+            else return null;
         }
         return null;
     }
