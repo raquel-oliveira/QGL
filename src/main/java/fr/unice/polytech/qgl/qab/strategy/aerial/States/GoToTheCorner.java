c1ef76fc69300d2e45397a632ea662def6e619e4
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-18 10:20:40 +0100

    class GoToTheCorner and include in the Initializa QAB-79

--- src/main/java/fr/unice/polytech/qgl/qab/strategy/aerial/States/GoToTheCorner.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/aerial/States/GoToTheCorner.java
@@ -1,7 +1,41 @@
 package fr.unice.polytech.qgl.qab.strategy.aerial.states;
 
+import fr.unice.polytech.qgl.qab.actions.Action;
+import fr.unice.polytech.qgl.qab.actions.aerial.combo.ComboFlyUntil;
+import fr.unice.polytech.qgl.qab.actions.common.Stop;
+import fr.unice.polytech.qgl.qab.exception.PositionOutOfMapaRange;
+import fr.unice.polytech.qgl.qab.map.Map;
+import fr.unice.polytech.qgl.qab.strategy.context.Context;
+import fr.unice.polytech.qgl.qab.strategy.context.UpdaterMap;
+
 /**
  * @version 17/12/15.
  */
-public class GoToTheCorner {
+public class GoToTheCorner extends AerialState {
+    private static GoToTheCorner instance;
+
+    private ComboFlyUntil actionCombo;
+    private UpdaterMap updaterMap;
+
+    private GoToTheCorner() {
+        super();
+        actionCombo = null;
+        updaterMap = new UpdaterMap();
+    }
+
+    public static GoToTheCorner getInstance() {
+        if (instance == null)
+            instance = new GoToTheCorner();
+        return instance;
+    }
+
+    @Override
+    public AerialState getState(Context context, Map map, StateMediator stateMediator) throws PositionOutOfMapaRange {
+        return null;
+    }
+
+    @Override
+    public Action responseState(Context context, Map map, StateMediator stateMediator) {
+        return new Stop();
+    }
 }
