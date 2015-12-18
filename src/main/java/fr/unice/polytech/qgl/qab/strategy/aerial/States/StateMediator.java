22980acb023cffa850a56c73c5d893fbb2075c20
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-18 10:18:29 +0100

    class statemediator QAB-79

--- src/main/java/fr/unice/polytech/qgl/qab/strategy/aerial/States/StateMediator.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/aerial/States/StateMediator.java
@@ -1,7 +1,62 @@
 package fr.unice.polytech.qgl.qab.strategy.aerial.states;
 
+import fr.unice.polytech.qgl.qab.util.enums.Direction;
+
 /**
  * @version 17/12/15.
  */
 public class StateMediator {
+    private static StateMediator instance;
+
+    // initialize - go to the corner
+    private boolean souldGoToTheCorner;
+    private int rangeToTheCorner;
+    private Direction directionToTheCorner;
+    private int rangeToGround;
+
+    private StateMediator() {
+        souldGoToTheCorner = false;
+        rangeToTheCorner = 0;
+        directionToTheCorner = null;
+        rangeToGround = 0;
+    }
+
+    public static StateMediator getInstance() {
+        if (instance == null)
+            instance = new StateMediator();
+        return instance;
+    }
+
+    public boolean shouldGoToTheCorner() {
+        return souldGoToTheCorner;
+    }
+
+    public void setGoToTheCorner(boolean goToTheCorner) {
+        this.souldGoToTheCorner = goToTheCorner;
+    }
+
+    public int getRangeToTheCorner() {
+        return rangeToTheCorner;
+    }
+
+    public void setRangeToTheCorner(int rangeToTheCorner) {
+        this.rangeToTheCorner = rangeToTheCorner;
+    }
+
+    public Direction getDirectionToTheCorner() {
+        return directionToTheCorner;
+    }
+
+    public void setDirectionToTheCorner(Direction directionToTheCorner) {
+        this.directionToTheCorner = directionToTheCorner;
+    }
+
+    public int getRangeToGround() {
+        return rangeToGround;
+    }
+
+    public void setRangeToGround(int rangeToGround) {
+        this.rangeToGround = rangeToGround;
+    }
+
 }
