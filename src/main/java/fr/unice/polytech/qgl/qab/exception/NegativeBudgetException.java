9e1ee509ce85df9e24fe35d2a09a6355a6ac7143
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2015-12-26 14:14:45 +0100

    Exception without caps lock. Fixed import

---
+++ src/main/java/fr/unice/polytech/qgl/qab/exception/NegativeBudgetException.java
@@ -0,0 +1,10 @@
+package fr.unice.polytech.qgl.qab.exception;
+
+/**
+ * @version 16/12/15.
+ */
+public class NegativeBudgetException extends Exception {
+    public NegativeBudgetException(String message){
+        super(message);
+    }
+}
