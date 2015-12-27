097cd92c28682d0284c0fad390769c0ef5681d4f
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-27 23:31:27 +0100

    fix the combo return

--- src/main/java/fr/unice/polytech/qgl/qab/actions/aerial/combo/ComboReturn.java
+++ src/main/java/fr/unice/polytech/qgl/qab/actions/aerial/combo/ComboReturn.java
@@ -25,98 +25,115 @@ public class ComboReturn extends Combo {
     }
 
     private void turnVertical(Direction head, Direction moveTo) {
-        if (head.isEquals(Direction.SOUTH) && moveTo.isEquals(Direction.EAST)) {
-            turnEastUp();
-            actions.add(new Echo(Direction.NORTH));
-            turnEastDown();
-            actions.add(new Echo(Direction.SOUTH));
-        }
-        else if (head.isEquals(Direction.SOUTH) && moveTo.isEquals(Direction.WEST)) {
-            turnWestUp();
-            actions.add(new Echo(Direction.NORTH));
-            turnWestDown();
-            actions.add(new Echo(Direction.SOUTH));
-        }
-        else if (head.isEquals(Direction.NORTH) && moveTo.isEquals(Direction.EAST)) {
-            turnEastDown();
-            actions.add(new Echo(Direction.SOUTH));
-            turnEastUp();
-            actions.add(new Echo(Direction.NORTH));
-        }
-        else if (head.isEquals(Direction.NORTH) && moveTo.isEquals(Direction.WEST)) {
-            turnWestDown();
-            actions.add(new Echo(Direction.SOUTH));
-            turnWestUp();
-            actions.add(new Echo(Direction.NORTH));
-        }
-        else if (head.isEquals(Direction.SOUTH) && moveTo.isEquals(Direction.SOUTH)) {
-            turnEastUp();
-            actions.add(new Echo(Direction.NORTH));
-            turnEastDown();
-            actions.add(new Echo(Direction.SOUTH));
-        }
-        else if (head.isEquals(Direction.NORTH) && moveTo.isEquals(Direction.NORTH)) {
-            turnEastDown();
-            actions.add(new Echo(Direction.SOUTH));
-            turnEastDown();
-            actions.add(new Echo(Direction.SOUTH));
+        if (head.isEquals(Direction.SOUTH)) {
+            turnBackHeadSouth(moveTo);
+        } else {
+            turnBackHeadNorth(moveTo);
         }
     }
 
     private void turnHorizontal(Direction head, Direction moveTo) {
-        if (head.isEquals(Direction.EAST) && moveTo.isEquals(Direction.SOUTH)) {
-            turnDowntoWest();
-            actions.add(new Echo(Direction.WEST));
-            turnDowntoEast();
-            actions.add(new Echo(Direction.EAST));
+        if (head.isEquals(Direction.EAST)) {
+            turnBackHeadEast(moveTo);
+        } else {
+            turnBackHeadWest(moveTo);
         }
-        else if (head.isEquals(Direction.WEST) && moveTo.isEquals(Direction.SOUTH)) {
-            turnDowntoEast();
-            actions.add(new Echo(Direction.EAST));
-            turnDowntoWest();
-            actions.add(new Echo(Direction.WEST));
-        }
-        else if (head.isEquals(Direction.SOUTH) && moveTo.isEquals(Direction.EAST)) {
+    }
+
+    private void turnDowntoEast() {
+        setDirections(Direction.SOUTH, Direction.EAST);
+    }
+
+    private void turnDowntoWest() {
+        setDirections(Direction.SOUTH, Direction.WEST);
+    }
+
+    private void turnEastUp() {
+        setDirections(Direction.EAST, Direction.NORTH);
+    }
+
+    private void turnEastDown() {
+        setDirections(Direction.EAST, Direction.SOUTH);
+    }
+
+    private void turnWestUp() {
+        setDirections(Direction.WEST, Direction.NORTH);
+    }
+
+    private void turnWestDown() {
+        setDirections(Direction.WEST, Direction.SOUTH);
+    }
+
+    private void turnUptoWest() {
+        setDirections(Direction.NORTH, Direction.WEST);
+    }
+
+    private void turnUptoEast() {
+        setDirections(Direction.NORTH, Direction.EAST);
+    }
+
+    public void turnBackHeadSouth(Direction moveTo) {
+        if (moveTo.isEquals(Direction.EAST)) {
             turnEastUp();
             actions.add(new Echo(Direction.NORTH));
             turnEastDown();
             actions.add(new Echo(Direction.SOUTH));
         }
-        else if (head.isEquals(Direction.SOUTH) && moveTo.isEquals(Direction.WEST)) {
+        else if (moveTo.isEquals(Direction.WEST)) {
             turnWestUp();
             actions.add(new Echo(Direction.NORTH));
             turnWestDown();
             actions.add(new Echo(Direction.SOUTH));
+        } 
+    }
+
+    public void turnBackHeadNorth(Direction moveTo) {
+        if (moveTo.isEquals(Direction.EAST)) {
+            turnEastDown();
+            actions.add(new Echo(Direction.SOUTH));
+            turnEastUp();
+            actions.add(new Echo(Direction.NORTH));
+        }
+        else if (moveTo.isEquals(Direction.WEST)) {
+            turnWestDown();
+            actions.add(new Echo(Direction.SOUTH));
+            turnWestUp();
+            actions.add(new Echo(Direction.NORTH));
+        } 
+    }
+
+    public void turnBackHeadEast(Direction moveTo) {
+        if (moveTo.isEquals(Direction.SOUTH)) {
+            turnDowntoWest();
+            actions.add(new Echo(Direction.WEST));
+            turnDowntoEast();
+            actions.add(new Echo(Direction.EAST));
+        }
+        else if (moveTo.isEquals(Direction.NORTH)) {
+            turnUptoWest();
+            actions.add(new Echo(Direction.EAST));
+            turnUptoEast();
+            actions.add(new Echo(Direction.WEST));
         }
     }
 
-    private void turnDowntoEast() {
-        actions.add(new Heading(Direction.SOUTH));
-        actions.add(new Heading(Direction.EAST));
+    private void turnBackHeadWest(Direction moveTo) {
+        if (moveTo.isEquals(Direction.SOUTH)) {
+            turnDowntoEast();
+            actions.add(new Echo(Direction.EAST));
+            turnDowntoWest();
+            actions.add(new Echo(Direction.WEST));
+        }
+        else if (moveTo.isEquals(Direction.NORTH)) {
+            turnUptoEast();
+            actions.add(new Echo(Direction.WEST));
+            turnUptoWest();
+            actions.add(new Echo(Direction.EAST));
+        }
     }
 
-    private void turnDowntoWest() {
-        actions.add(new Heading(Direction.SOUTH));
-        actions.add(new Heading(Direction.WEST));
-    }
-
-    private void turnEastUp() {
-        actions.add(new Heading(Direction.EAST));
-        actions.add(new Heading(Direction.NORTH));
-    }
-
-    private void turnEastDown() {
-        actions.add(new Heading(Direction.EAST));
-        actions.add(new Heading(Direction.SOUTH));
-    }
-
-    private void turnWestUp() {
-        actions.add(new Heading(Direction.WEST));
-        actions.add(new Heading(Direction.NORTH));
-    }
-
-    private void turnWestDown() {
-        actions.add(new Heading(Direction.WEST));
-        actions.add(new Heading(Direction.SOUTH));
+    public void setDirections(Direction d1, Direction d2) {
+        actions.add(new Heading(d1));
+        actions.add(new Heading(d2));
     }
 }
