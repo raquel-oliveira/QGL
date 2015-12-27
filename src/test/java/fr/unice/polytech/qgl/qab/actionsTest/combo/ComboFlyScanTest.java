4baecd872cb1673842d1e8db9bf72f490ec03b0e
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-27 23:18:38 +0100

    add test fly scan QAB-70

--- src/test/java/fr/unice/polytech/qgl/qab/actionsTest/combo/ComboFlyScanTest.java
+++ src/test/java/fr/unice/polytech/qgl/qab/actionsTest/combo/ComboFlyScanTest.java
@@ -1,7 +1,29 @@
 package fr.unice.polytech.qgl.qab.actionsTest.combo;
 
+import fr.unice.polytech.qgl.qab.actions.aerial.Fly;
+import fr.unice.polytech.qgl.qab.actions.aerial.Scan;
+import fr.unice.polytech.qgl.qab.actions.aerial.combo.ComboFlyScan;
+import fr.unice.polytech.qgl.qab.exception.IndexOutOfBoundsComboAction;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * @version 27/12/15.
  */
 public class ComboFlyScanTest {
+    ComboFlyScan combo;
+
+    @Before
+    public void defineContext() {
+        combo = new ComboFlyScan();
+    }
+
+    @Test
+    public void testDefine() throws IndexOutOfBoundsComboAction {
+        combo.defineActions();
+        assertEquals(Fly.class, ((Fly)combo.get(0)).getClass());
+        assertEquals(Scan.class, ((Scan)combo.get(1)).getClass());
+    }
 }
