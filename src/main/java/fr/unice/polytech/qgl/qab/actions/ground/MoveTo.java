ab9e29f474f3fba60b9daf07317a9a141297f808
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2015-12-31 14:34:32 +0100

    add action moveTo QAB-90

---
+++ src/main/java/fr/unice/polytech/qgl/qab/actions/ground/MoveTo.java
@@ -0,0 +1,27 @@
+package fr.unice.polytech.qgl.qab.actions.ground;
+
+import fr.unice.polytech.qgl.qab.actions.Action;
+import fr.unice.polytech.qgl.qab.util.enums.Direction;
+import org.json.JSONObject;
+
+/**
+ * @version 31/12/15.
+ */
+public class MoveTo implements Action {
+    private Direction direction;
+
+    public MoveTo() {
+        super();
+        direction = direction;
+    }
+
+    @Override
+    public boolean isValid(JSONObject jsonObj) {
+        return true;
+    }
+
+    @Override
+    public String formatResponse() {
+        return "{ \"action\": \"move_to\", \"parameters\": { \"direction\": \"" + direction + "\" } }";
+    }
+}
