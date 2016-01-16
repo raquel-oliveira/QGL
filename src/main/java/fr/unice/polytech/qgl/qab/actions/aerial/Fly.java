0c646ff8dd8bb5f028817fe6085d9464d6733072
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-01-16 20:06:41 +0100

    IsValid to Fly  #QAB-91

--- src/main/java/fr/unice/polytech/qgl/qab/actions/aerial/Fly.java
+++ src/main/java/fr/unice/polytech/qgl/qab/actions/aerial/Fly.java
@@ -8,7 +8,7 @@ import org.json.JSONObject;
  *
  * @version 4.9
  */
-public class Fly implements Action {
+public class Fly extends Action {
 
     private static final String ACTION_FLY = "fly";
 
@@ -16,16 +16,14 @@ public class Fly implements Action {
         super();
     }
 
-    @Override
     public boolean isValid(JSONObject jsonObj) {
-        if (jsonObj.has("action")) {
-            String action = jsonObj.getString("action");
+        if (jsonObj.has(ACTION)) {
+            String action = jsonObj.getString(ACTION);
             return ACTION_FLY.equals(action);
         }
         return false;
     }
 
-    @Override
     public String formatResponse() {
         return "{ \"action\": \"fly\" }";
     }
