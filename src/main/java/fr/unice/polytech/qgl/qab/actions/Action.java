14de6ba2aec0ea9ffe71cadedada94e71ba18797
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-01-16 20:00:39 +0100

    Action class to abstract

--- src/main/java/fr/unice/polytech/qgl/qab/actions/Action.java
+++ src/main/java/fr/unice/polytech/qgl/qab/actions/Action.java
@@ -1,5 +1,6 @@
 package fr.unice.polytech.qgl.qab.actions;
 
+import fr.unice.polytech.qgl.qab.util.enums.Direction;
 import org.json.JSONObject;
 
 /**
@@ -7,9 +8,25 @@ import org.json.JSONObject;
  *
  * @version 4.9
  */
-public interface Action {
-    boolean isValid(JSONObject jsonObj);
+public abstract class Action {
+    public static final String ACTION = "action";
+    public static final String DIRECTION = "direction";
+    public static final String PARAMETERS = "parameters";
 
-    String formatResponse();
+    public abstract boolean isValid(JSONObject jsonObj);
+
+    public abstract String formatResponse();
+
+    /**
+     *Check parameters if the parameter is the direction
+     */
+    public boolean checkParameter(JSONObject jsonObj){
+        if (jsonObj.has(PARAMETERS)) {
+            if (jsonObj.getJSONObject(PARAMETERS).has(DIRECTION)) {
+                String dir = jsonObj.getJSONObject(PARAMETERS).getString(DIRECTION);
+                return !(Direction.fromString(dir) == null);
+            } else return false;
+        }
+        else return false;
+    }
 }
-
