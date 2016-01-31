07503e770eaec9906d2a0ff7005f3d3c04235f52
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-01-31 03:44:23 +0100

    add scout QAB-27 and test QAB-70

--- src/test/java/fr/unice/polytech/qgl/qab/actionsTest/aerial/HeadingTest.java
+++ src/test/java/fr/unice/polytech/qgl/qab/actionsTest/aerial/HeadingTest.java
@@ -9,11 +9,12 @@ import org.junit.Test;
 
 import java.lang.reflect.Field;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Created by Raquel on 27/12/2015.
+ * @version  27/12/2015.
  */
 public class HeadingTest {
     Heading heading;
@@ -48,27 +49,27 @@ public class HeadingTest {
     @Test
     public void testInvalideDirection() {
         JSONObject jsonObj = new JSONObject("{ \"action\": \"heading\", \"parameters\": { \"direction\": \"A\" }}");
-        assertTrue(!heading.isValid(jsonObj));
+        assertFalse(heading.isValid(jsonObj));
     }
 
     @Test
     public void testNotValidJson() {
         JSONObject jsonObj = new JSONObject("{ \"notAction\": \"heading\", \"parameters\": { \"direction\": \"E\" }}");
-        assertTrue(!(heading.isValid(jsonObj)));
+        assertFalse(heading.isValid(jsonObj));
     }
 
     @Test
     public void testNotValidActionJson() {
         JSONObject jsonObj = new JSONObject("{\"action\": \"notHeading\", \"parameters\": { \"direction\": \"E\" }}");
-        assertTrue(!(heading.isValid(jsonObj)));
+        assertFalse(heading.isValid(jsonObj));
     }
 
     @Test
     public void noParametersTest() {
         String response = "{ \"action\": \"heading\"}";
         JSONObject jsonObj = new JSONObject(response);
-        assertTrue(!(heading.isValid(jsonObj)));
-        assertTrue(!(heading.formatResponse().equals(response)));
+        assertFalse(heading.isValid(jsonObj));
+        assertFalse(heading.formatResponse().equals(response));
     }
 
     @Test
@@ -85,5 +86,10 @@ public class HeadingTest {
         JSONObject jsonObj = new JSONObject(response);
         assertTrue((heading.isValid(jsonObj)));
         assertTrue((heading.formatResponse().equals(response)));
+    }
+
+    @Test
+    public void getDirectionTest() {
+        assertEquals(dir, heading.getDirection());
     }
 }
