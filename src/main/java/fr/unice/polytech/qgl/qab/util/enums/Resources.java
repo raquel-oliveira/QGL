efe7491dd9683ef0aa1d34bb1843de993e77c1a4
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-02-01 16:02:16 +0100

    Primary resources, get biome of resources #QAB-84

--- src/main/java/fr/unice/polytech/qgl/qab/util/enums/Resources.java
+++ src/main/java/fr/unice/polytech/qgl/qab/util/enums/Resources.java
@@ -1,81 +1,78 @@
 package fr.unice.polytech.qgl.qab.util.enums;
 
+import fr.unice.polytech.qgl.qab.resources.Resource;
 import fr.unice.polytech.qgl.qab.util.enums.Biomes;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Raquel on 20/12/2015.
+ * version 01.01.2016
  */
 public enum Resources {
-    FISH("FISH"),
-    FLOWER("FLOWER"),
-    FRUITS("FRUITS"),
-    FUR("FUR"),
-    ORE("ORE"),
-    QUARTZ("QUARTZ"),
-    SUGAR_CANE("SUGAR_CANE"),
-    WOOD("WOOD"),
+    FISH,
+    FLOWER,
+    FRUITS,
+    FUR,
+    ORE,
+    QUARTZ,
+    SUGAR_CANE,
+    WOOD;
 
-    GLASS("GLASS"),
-    INGOT("INGOT"),
-    LEATHER("LEATHER"),
-    PLANK("PLANK"),
-    RUM("RUM");
+    private Set<Biomes> biomes = new HashSet<>(); //todo: change to static?
 
-    private String name = "";
-    private List<Biomes> biomes = new ArrayList<>();
-
-    Resources(String name) {
-        this.name = name;
-        if (this.name == "GLASS") {
-            biomes.add(Biomes.OCEAN);
-            biomes.add(Biomes.LAKE);
+    /**
+     * Coonstructor of Resource
+     */
+    Resources() {
+        switch (this){
+            case FISH:
+                biomes.add(Biomes.OCEAN);
+                biomes.add(Biomes.LAKE);
+                break;
+            case FLOWER:
+                biomes.add(Biomes.ALPINE);
+                biomes.add(Biomes.GLACIER);
+                biomes.add(Biomes.MANGROVE);
+                break;
+            case FRUITS:
+                biomes.add(Biomes.MANGROVE); //tropical biomes != tropical forest?
+                biomes.add(Biomes.TROPICAL_RAIN_FOREST);
+                biomes.add(Biomes.TROPICAL_SEASONAL_FOREST);
+                break;
+            case FUR:
+                biomes.add(Biomes.GRASSLAND);
+                biomes.add(Biomes.TEMPERATE_RAIN_FOREST);
+                break;
+            case ORE:
+                biomes.add(Biomes.ALPINE);
+                biomes.add(Biomes.SUB_TROPICAL_DESERT);
+                biomes.add(Biomes.TEMPERATE_DESERT);
+                break;
+            case QUARTZ:
+                biomes.add(Biomes.BEACH);
+                biomes.add(Biomes.SUB_TROPICAL_DESERT);
+                biomes.add(Biomes.TEMPERATE_DESERT);
+                break;
+            case SUGAR_CANE:
+                biomes.add(Biomes.MANGROVE); //tropical biomes != tropical forest?
+                biomes.add(Biomes.TROPICAL_RAIN_FOREST);
+                biomes.add(Biomes.TROPICAL_SEASONAL_FOREST);
+                break;
+            case WOOD:
+                biomes.add(Biomes.MANGROVE);
+                biomes.add(Biomes.TEMPERATE_DECIDUOUS_FOREST);
+                biomes.add(Biomes.TEMPERATE_RAIN_FOREST);
+                biomes.add(Biomes.TROPICAL_RAIN_FOREST);
+                biomes.add(Biomes.TROPICAL_SEASONAL_FOREST);
+                break;
         }
+
     }
 
-    public boolean isManufactured(){
-        return this == GLASS || this == INGOT || this == LEATHER || this == PLANK || this == RUM;
-    }
-
-
-    /**
-     * Method to return the list of the actions (echos).
-     * @return actions list
-     */
-    public List<Biomes> getActions() {
+    public Set<Biomes> getBiome(){
         return biomes;
-    }
-    /**
-     * Method that check if the action list is empty
-     * @return true if the list is empty, false if not
-     */
-    public boolean isEmpty() {
-        return biomes.isEmpty();
-    }
-
-    /**
-     * Return one action in a defined index.
-     * @param index of the action to return
-     * @return action chosen
-     */
-    public Biomes get(int index) {
-        return biomes.get(index);
-    }
-
-    /**
-     * Method to remove a item that is in the index gave.
-     * @param index to remove item
-     * @return the item removed
-     */
-    public Biomes remove(int index) {
-        return biomes.remove(index);
-    }
-
-    @Override
-    public String toString() {
-        return name;
     }
 
 }
