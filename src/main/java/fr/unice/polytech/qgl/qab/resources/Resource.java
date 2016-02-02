990b03f40a083055012f63d2efd173f038304de8
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-02-02 21:29:31 +0100

    interface resource QAB-96

--- src/main/java/fr/unice/polytech/qgl/qab/resources/Resource.java
+++ src/main/java/fr/unice/polytech/qgl/qab/resources/Resource.java
@@ -1,46 +1,9 @@
 package fr.unice.polytech.qgl.qab.resources;
 
+
 /**
  * @version 16/12/15.
  */
-public class Resource {
-    private String name;
-    private int amount;
-    private int contract;
-
-    public Resource(String name) {
-        this.name = name;
-        this.amount = 0;
-        this.contract = 0;
-    }
-
-    public Resource(String name, int amount, int contract) {
-        this.name = name;
-        this.amount = amount;
-        this.contract = contract;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
-
-    public void setContract(int contract) {
-        this.contract = contract;
-    }
-
-    public int getContract() {
-        return contract;
-    }
-
-    public void increment(int amount) {
-        this.amount += amount;
-    }
+public interface Resource {
+    String getName();
 }
--- src/test/java/fr/unice/polytech/qgl/qab/resource/ResourceTest.java
+++ src/test/java/fr/unice/polytech/qgl/qab/resource/ResourceTest.java
@@ -1,5 +1,7 @@
 package fr.unice.polytech.qgl.qab.resource;
 
+import fr.unice.polytech.qgl.qab.resources.primary.PrimaryResource;
+import fr.unice.polytech.qgl.qab.resources.primary.PrimaryType;
 import fr.unice.polytech.qgl.qab.resources.Resource;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,31 +16,14 @@ public class ResourceTest {
 
     @Before
     public void defineContext() {
-        resource = new Resource("FISH");
+        resource = new PrimaryResource(PrimaryType.FISH);
     }
 
     @Test
     public void testGoodObject() {
         assertEquals("FISH", resource.getName());
 
-        resource = new Resource("FISH", 10, 10);
-        assertEquals("FISH", resource.getName());
-        assertEquals(10, resource.getAmount());
-        assertEquals(10, resource.getContract());
-    }
-
-    @Test
-    public void testSets() {
-        resource.setAmount(20);
-        assertEquals(20, resource.getAmount());
-
-        resource.setContract(20);
-        assertEquals(20, resource.getContract());
-    }
-
-    @Test
-    public void testIncrement() {
-        resource.increment(40);
-        assertEquals(40, resource.getAmount());
+        resource = new PrimaryResource(PrimaryType.FUR);
+        assertEquals("FUR", resource.getName());
     }
 }
