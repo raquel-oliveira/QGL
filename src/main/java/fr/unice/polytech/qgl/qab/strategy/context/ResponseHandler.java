7ca40061ba48aa67c10e2a63766c9209d593ddd3
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-02-06 02:26:53 +0100

    stock the response data of the glimpse QAB-102

--- src/main/java/fr/unice/polytech/qgl/qab/strategy/context/ResponseHandler.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/context/ResponseHandler.java
@@ -4,15 +4,19 @@ import fr.unice.polytech.qgl.qab.actions.Action;
 import fr.unice.polytech.qgl.qab.actions.simple.aerial.Echo;
 import fr.unice.polytech.qgl.qab.actions.simple.aerial.Fly;
 import fr.unice.polytech.qgl.qab.actions.simple.aerial.Scan;
+import fr.unice.polytech.qgl.qab.actions.simple.ground.Glimpse;
 import fr.unice.polytech.qgl.qab.exception.NegativeBudgetException;
 import fr.unice.polytech.qgl.qab.map.tile.Biomes;
 import fr.unice.polytech.qgl.qab.map.tile.Creek;
+import fr.unice.polytech.qgl.qab.response.GlimpseResponse;
 import fr.unice.polytech.qgl.qab.util.Discovery;
 import fr.unice.polytech.qgl.qab.util.enums.Found;
+import fr.unice.polytech.qgl.qab.util.enums.TypeBiome;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -46,6 +50,8 @@ public class ResponseHandler {
             tempContext = readDataFromScan(tempContext, jsonObj);
         } else if (takeAction instanceof Fly) {
             tempContext.getLastDiscovery().setUp();
+        } else if (takeAction instanceof Glimpse) {
+            tempContext = readDataFromGlimpse(contextIsland, jsonObj);
         }
 
         return tempContext;
@@ -84,6 +90,52 @@ public class ResponseHandler {
 
         discovery.setBiomes(biomes);
         discovery.setCreeks(creeks);
+        context.setLastDiscovery(discovery);
+
+        return context;
+    }
+
+    private Context readDataFromGlimpse(Context context, JSONObject jsonObj) {
+        GlimpseResponse glimpseResponse = new GlimpseResponse();
+
+        if(jsonObj.getJSONObject(EXTRAS).has("asked_range")) {
+            int range = jsonObj.getJSONObject(EXTRAS).getInt("asked_range");
+            glimpseResponse.setAsked_range(range);
+        }
+
+        JSONArray jsonArray;
+
+        List<HashMap<Biomes, Double>> initial_tiles = new ArrayList<>();
+        List<Biomes> third = new ArrayList<>();
+        Biomes fourth = null;
+
+        if(jsonObj.getJSONObject(EXTRAS).has("report")) {
+            jsonArray = jsonObj.getJSONObject(EXTRAS).getJSONArray("report");
+            for (int j = 0; j < 2; j++) {
+                HashMap<Biomes, Double> tile = new HashMap<>();
+                JSONArray jarray = jsonArray.getJSONArray(j);
+                for (int i = 0; i < jarray.length(); i++) {
+                    JSONArray item = jarray.getJSONArray(i);
+                    String bio = item.getString(0);
+                    double percentage = item.getDouble(1);
+                    tile.put(Biomes.valueOf(bio), percentage);
+                }
+                initial_tiles.add(tile);
+            }
+
+            JSONArray jarray = jsonArray.getJSONArray(2);
+            for (int i = 0; i < jarray.length(); i++) {
+                third.add(Biomes.valueOf(jarray.getString(i)));
+            }
+
+            fourth = Biomes.valueOf(jsonArray.getJSONArray(3).getString(0));
+        }
+
+        glimpseResponse.setInitial_tiles(initial_tiles);
+        glimpseResponse.setThird_tile(third);
+        glimpseResponse.setFourth_tile(fourth);
+
+        discovery.setGlimpseResponse(glimpseResponse);
         context.setLastDiscovery(discovery);
 
         return context;
--- src/main/java/fr/unice/polytech/qgl/qab/strategy/ground/states/MoveInTheGround.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/ground/states/MoveInTheGround.java
@@ -1,12 +1,25 @@
 package fr.unice.polytech.qgl.qab.strategy.ground.states;
 
 import fr.unice.polytech.qgl.qab.actions.Action;
+import fr.unice.polytech.qgl.qab.actions.combo.ground.ComboMoveTo;
 import fr.unice.polytech.qgl.qab.actions.simple.common.Stop;
-import fr.unice.polytech.qgl.qab.actions.simple.ground.MoveTo;
+import fr.unice.polytech.qgl.qab.actions.simple.ground.Exploit;
+import fr.unice.polytech.qgl.qab.actions.simple.ground.Explore;
+import fr.unice.polytech.qgl.qab.actions.simple.ground.Glimpse;
 import fr.unice.polytech.qgl.qab.exception.IndexOutOfBoundsComboAction;
 import fr.unice.polytech.qgl.qab.exception.PositionOutOfMapRange;
 import fr.unice.polytech.qgl.qab.map.Map;
+import fr.unice.polytech.qgl.qab.map.tile.Biomes;
+import fr.unice.polytech.qgl.qab.resources.Resource;
+import fr.unice.polytech.qgl.qab.resources.primary.PrimaryResource;
+import fr.unice.polytech.qgl.qab.resources.primary.PrimaryType;
+import fr.unice.polytech.qgl.qab.response.GlimpseResponse;
 import fr.unice.polytech.qgl.qab.strategy.context.Context;
+import fr.unice.polytech.qgl.qab.strategy.context.ContractItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @version 31/12/15.
@@ -14,9 +27,12 @@ import fr.unice.polytech.qgl.qab.strategy.context.Context;
 public class MoveInTheGround extends GroundState {
     private static MoveInTheGround instance;
 
+    private ComboMoveTo actionCombo;
+
     private MoveInTheGround() {
         super();
         this.lastAction = null;
+        actionCombo = null;
     }
 
     public static MoveInTheGround getInstance() {
@@ -27,18 +43,80 @@ public class MoveInTheGround extends GroundState {
 
     @Override
     public GroundState getState(Context context, Map map, StateManager stateManager) throws PositionOutOfMapRange {
-        if(stateManager.getDecideToExit()){
-            return Exit.getInstance();
-        }
-        stateManager.setDecideToExit();
         return MoveInTheGround.getInstance();
     }
 
     @Override
     public Action responseState(Context context, Map map, StateManager stateManager) throws IndexOutOfBoundsComboAction {
-        if (lastAction == null)
-            return new MoveTo(context.getHeading());
-        else
-            return new Stop();
+        Action act;
+
+        if (lastAction == null) {
+            act = new Glimpse(context.getHeading(), 4);
+            lastAction = act;
+            return act;
+        } else if (lastAction instanceof Glimpse) {
+            List<Boolean> resources = biomeAnalyzer(context);
+            for (int i = 0; i < resources.size(); i++) {
+                if (i == 0 && resources.get(i)) {
+                    act = new Explore();
+                    lastAction = act;
+                    return act;
+                }
+            }
+        } else if (lastAction instanceof Explore) {
+            Resource res = new PrimaryResource(PrimaryType.FISH);
+            act = new Exploit(res);
+            lastAction = act;
+            return act;
+        }
+        return new Stop();
+    }
+
+    public List<Boolean> biomeAnalyzer(Context context) {
+        // the glimpse response
+        GlimpseResponse gr = context.getLastDiscovery().getGlimpseResponse();
+        // the contract info
+        List<ContractItem> contract = context.getContracts();
+
+        // the initial tiles info
+        List<HashMap<Biomes, Double>> initial_tiles = gr.getInitial_tiles();
+        // if for each tile there is one or more good biome
+        List<Boolean> goodTiles = new ArrayList<>();
+
+        int index_tile = 0;
+
+        // two firts tiles
+        for (HashMap<Biomes, Double> tile: initial_tiles) {
+            for (Biomes key : tile.keySet()) {
+                for (ContractItem item: contract) {
+                    if (item.resource().getBiome().contains(key)) {
+                        goodTiles.add(index_tile, true);
+                        break;
+                    }
+                }
+                if (goodTiles.get(index_tile)) break;
+            }
+            index_tile++;
+        }
+
+        List<Biomes> third_tile = gr.getThird_tile();
+        for (Biomes key : third_tile) {
+            for (ContractItem item: contract) {
+                if (item.resource().getBiome().contains(key)) {
+                    goodTiles.add(index_tile++, true);
+                    break;
+                }
+            } if (goodTiles.get(index_tile-1)) break;
+        }
+
+        Biomes fourth = gr.getFourth_tile();
+        for (ContractItem item: contract) {
+            if (item.resource().getBiome().contains(fourth)) {
+                goodTiles.add(index_tile++, true);
+                break;
+            }
+        }
+
+        return goodTiles;
     }
 }
