4d1e61202d938f4240a6edd0e161da00237d7635
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-02-07 16:34:51 +0100

    add explore response test QAB-110

--- src/main/java/fr/unice/polytech/qgl/qab/response/ExploreResponse.java
+++ src/main/java/fr/unice/polytech/qgl/qab/response/ExploreResponse.java
@@ -3,7 +3,6 @@ package fr.unice.polytech.qgl.qab.response;
 import fr.unice.polytech.qgl.qab.resources.Resource;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -16,12 +15,11 @@ public class ExploreResponse {
         resources = new ArrayList<>();
     }
 
-    public void addResource(List<String> data_resource) {
-        resources.add(data_resource);
+    public void addResource(List<String> dataResource) {
+        resources.add(dataResource);
     }
 
     public boolean contains(Resource resource) {
-        List<Resource> res = new ArrayList<>();
         for (int i = 0; i < resources.size(); i++) {
             if (resources.get(i).get(1).equals(resource.getName()))
                 return true;
