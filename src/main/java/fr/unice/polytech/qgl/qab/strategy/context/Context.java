627a99b8bc533df39c2f71671ab2d279a94569d8
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-02-14 12:38:29 +0100

    add comments of the context (budget, context, contract, response, update map) QAB-112

--- src/main/java/fr/unice/polytech/qgl/qab/strategy/context/Context.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/context/Context.java
@@ -14,17 +14,28 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @version 16/12/15.
+ *
+ * Class that represents the context of the simulation.
+ */
 public class Context {
     private int men;
     private boolean status;
     private Budget budget;
     private List<ContractItem> contracts;
 
+    // direction of the head in the begin
     private Direction firstHead;
+    // direction of the current head
     private Direction heading;
 
     private Discovery lastDiscovery;
 
+    /**
+     * Context's constructor
+     * @throws NegativeBudgetException exception if the valut to the budget is negative
+     */
     public Context() throws NegativeBudgetException {
         men = 0;
         status = true;
@@ -37,6 +48,11 @@ public class Context {
         lastDiscovery = null;
     }
 
+    /**
+     * Method to read the string context (json).
+     * @param context value of the context of the simulation
+     * @throws NegativeBudgetException
+     */
     public void read(String context) throws NegativeBudgetException {
         JSONObject jsonObj = new JSONObject(context);
 
@@ -55,50 +71,101 @@ public class Context {
         setHeading(firstHead);
     }
 
+    /**
+     * Get the direction of the head
+     * @return direction of the head
+     */
     public Direction getHeading() {
         return heading;
     }
 
+    /**
+     * Get the value of the budget
+     * @return value of the budget
+     */
     public int getBudget() {
         return budget.remaining();
     }
 
+    /**
+     * Get the direction of the head initial
+     * @return direction of the head initial
+     */
     public Direction getFirstHead() {
         return firstHead;
     }
 
+    /**
+     * Get the last discovery
+     * @return the last discovery
+     */
     public Discovery getLastDiscovery() {
         return lastDiscovery;
     }
 
+    /**
+     * Get the status
+     * @return status
+     */
     public boolean getStatus() {
         return status;
     }
 
+    /**
+     * Get the number of men was gave in the begin of the simulation
+     * @return the number of men
+     */
     public int getMen() {
         return men;
     }
 
+    /**
+     * The the list of contracts gave in the begin of the simulation
+     * @return list of contracts
+     */
     public List<ContractItem> getContracts() {
         return contracts;
     }
 
+    /**
+     * Set the status value
+     * @param s status value
+     */
     public void setStatus(boolean s) {
         status = s;
     }
 
+    /**
+     * Set the direction of the first head
+     * @param direction value of the first head
+     */
     public void setFirstHead(Direction direction) {
         firstHead = direction;
     }
 
+    /**
+     * Set the value to the budget
+     * @param b budget value
+     * @throws NegativeBudgetException
+     */
     public void setBudget(int b) throws NegativeBudgetException {
         budget = new Budget(b);
     }
 
+    /**
+     * Set the number of men
+     * @param m number of men
+     */
     public void setMen(int m) {
         men = m;
     }
 
+    /**
+     * Add items in the contract
+     * @param resource resource's name
+     * @param amount resource's amount
+     * @throws NegativeBudgetException
+     */
     public void addContract(String resource, int amount) throws NegativeBudgetException {
         try {
             contracts.add(new ContractItem(new ManufacturedResource(ManufacturedType.valueOf(resource)), amount));
@@ -107,10 +174,18 @@ public class Context {
         }
     }
 
+    /**
+     * Set the direction of the head
+     * @param dir direction of the head
+     */
     public void setHeading(Direction dir) {
         heading = dir;
     }
 
+    /**
+     * Set last discovery
+     * @param lastDiscovery last discovery
+     */
     public void setLastDiscovery(Discovery lastDiscovery) {
         this.lastDiscovery = lastDiscovery;
     }
