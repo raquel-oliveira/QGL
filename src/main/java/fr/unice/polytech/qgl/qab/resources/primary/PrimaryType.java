cae669723d63f98de1ef74a983ef2b5e3ce83b56
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-02-14 14:00:15 +0100

    add comments of the resources QAB-112

--- src/main/java/fr/unice/polytech/qgl/qab/resources/primary/PrimaryType.java
+++ src/main/java/fr/unice/polytech/qgl/qab/resources/primary/PrimaryType.java
@@ -1,8 +1,9 @@
 package fr.unice.polytech.qgl.qab.resources.primary;
 
 /**
- * Created by Raquel on 20/12/2015.
- * @version 01/02/2016
+ * @version 20/12/2015
+ *
+ * Enum that represents the types of the primary resources
  */
 public enum PrimaryType {
     FISH, FLOWER, FRUITS, FUR, ORE, QUARTZ, SUGAR_CANE, WOOD
