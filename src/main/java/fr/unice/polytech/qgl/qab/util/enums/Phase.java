1f848a9d4bcf877e53c297cfc08b4844616bdebc
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-02-29 16:50:11 +0100

    Refactoring of enum Phase QAB-111

--- src/main/java/fr/unice/polytech/qgl/qab/util/enums/Phase.java
+++ src/main/java/fr/unice/polytech/qgl/qab/util/enums/Phase.java
@@ -4,16 +4,6 @@ package fr.unice.polytech.qgl.qab.util.enums;
  * @version 01/12/15.
  */
 public enum Phase {
-    GROUND("GROUND"),
-    AERIAL("AERIAL");
-
-    private String name = "";
-
-    Phase(String name) {
-            this.name = name;
-    }
-
-    public boolean isEquals(Phase phase) {
-        return phase.toString().equalsIgnoreCase(name);
-    }
+    GROUND,
+    AERIAL;
 }
