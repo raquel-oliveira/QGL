9af53875a8020a87e5f3a6f194c6502c88d115a7
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-03-07 22:13:50 +0100

    Implementation of Action Transform and formatResponse of Action Transform #QAB-115

--- src/main/java/fr/unice/polytech/qgl/qab/actions/simple/ground/Transform.java
+++ src/main/java/fr/unice/polytech/qgl/qab/actions/simple/ground/Transform.java
@@ -1,45 +1,25 @@
 package fr.unice.polytech.qgl.qab.actions.simple.ground;
 
 import fr.unice.polytech.qgl.qab.actions.Action;
-import fr.unice.polytech.qgl.qab.resources.manufactured.ManufacturedResource;
-import fr.unice.polytech.qgl.qab.resources.primary.PrimaryResource;
 import fr.unice.polytech.qgl.qab.resources.primary.PrimaryType;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Version 02/03/2016.
  */
-public class Transform extends Action{
+public class Transform extends Action {
     private static final String ACTION_TRANSFORM = "transform";
     private Map<PrimaryType, Integer> recipe;
-    private List<String> recipeIngredients;
 
     /**
-     * Constructor to transform' given the values of the recipe.
+     * Constructor to transform given the values of the recipe.
      * @param recipe
      */
     public Transform(Map<PrimaryType, Integer> recipe){
-        super();
         this.recipe = recipe;
-        for (PrimaryType name: recipe.keySet()){
-            recipeIngredients.add(name.name());
-        }
-    }
-
-    /**
-     * Constructor transfom to create 1(ONE) resource manufactured.
-     * @param resource
-     */
-    public Transform(ManufacturedResource resource){
-        super();
-        this.recipe = resource.getRecipe(1);
-        for (PrimaryType name: recipe.keySet()){
-            recipeIngredients.add(name.name());
-        }
     }
 
     @Override
@@ -55,9 +35,22 @@ public class Transform extends Action{
     }
 
     @Override
-    public String formatResponse(){
-        //TODO: check this
-        return "{ \"action\": \"transform\", \"parameters\": {" + recipeIngredients.get(0) +":"+ recipe.get(recipeIngredients.get(0))+ "} }";
+    public String formatResponse() {
+        JSONObject response = new JSONObject();
+        response.put("action", "transform");
+
+        JSONArray parameters = new JSONArray();
+        for (Map.Entry<PrimaryType, Integer> resources: recipe.entrySet()) {
+            PrimaryType type = resources.getKey();
+            Integer amount = resources.getValue();
+
+            JSONObject param = new JSONObject();
+            param.put(type.name(), amount.toString());
+            parameters.put(param);
+        }
+
+        response.put("parameters", parameters);
+        return response.toString();
     }
 
 }
