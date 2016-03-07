9673c60516c29ebee6de03f3ea81806171c47708
huang xiaohan <stephanie1028han@gmail.com>
2016-03-07 15:24:26 +0100

    add test code of ComboScout QAB-150

--- src/main/java/fr/unice/polytech/qgl/qab/actions/combo/ground/ComboScout.java
+++ src/main/java/fr/unice/polytech/qgl/qab/actions/combo/ground/ComboScout.java
@@ -1,7 +1,6 @@
 package fr.unice.polytech.qgl.qab.actions.combo.ground;
 
 import fr.unice.polytech.qgl.qab.actions.combo.Combo;
-import fr.unice.polytech.qgl.qab.actions.simple.ground.MoveTo;
 import fr.unice.polytech.qgl.qab.actions.simple.ground.Scout;
 import fr.unice.polytech.qgl.qab.util.enums.Direction;
 
@@ -23,6 +22,6 @@ public class ComboScout extends Combo {
         Direction dir = Direction.randomSideDirection(direction);
         actions.add(new Scout(dir));
         actions.add(new Scout(Direction.inverse(dir)));
-        actions.add(new Scout(Direction.inverse(direction)));
+        actions.add(new Scout(direction));
     }
 }
---
+++ src/test/java/fr/unice/polytech/qgl/qab/actions/combo/ground/ComboScoutTest.java
@@ -0,0 +1,36 @@
+package fr.unice.polytech.qgl.qab.actions.combo.ground;
+
+import fr.unice.polytech.qgl.qab.exception.IndexOutOfBoundsComboAction;
+import fr.unice.polytech.qgl.qab.util.enums.Direction;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by huang on 07/03/16.
+ */
+public class ComboScoutTest {
+    public ComboScout comboScout;
+
+    @Before
+    public void defineComboScout(){
+        comboScout = new ComboScout();
+    }
+
+    @Test
+    public void testDefineAction() throws IndexOutOfBoundsComboAction {
+        assertEquals(ComboScout.class, comboScout.getClass());
+
+        comboScout.defineActions(Direction.EAST);
+        assertTrue(comboScout.get(0).getDirection() == Direction.NORTH
+                || comboScout.get(0).getDirection() == Direction.SOUTH);
+
+        assertTrue(comboScout.get(1).getDirection() == Direction.SOUTH
+                || comboScout.get(1).getDirection() == Direction.NORTH);
+
+        assertEquals(Direction.EAST, comboScout.get(2).getDirection());
+
+    }
+}
