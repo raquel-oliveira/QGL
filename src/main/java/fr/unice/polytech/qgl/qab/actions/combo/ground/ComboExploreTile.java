d28716397c9e645846b692005764118edd33a595
Gabriela Cavalcante <gabicavalcantesilva@gmail.com>
2016-03-20 13:12:40 +0100

    fix Technical Debt QAB-152

--- src/main/java/fr/unice/polytech/qgl/qab/actions/combo/ground/ComboExploreTile.java
+++ src/main/java/fr/unice/polytech/qgl/qab/actions/combo/ground/ComboExploreTile.java
@@ -2,8 +2,7 @@ package fr.unice.polytech.qgl.qab.actions.combo.ground;
 
 import fr.unice.polytech.qgl.qab.actions.combo.Combo;
 import fr.unice.polytech.qgl.qab.actions.simple.ground.Explore;
-import fr.unice.polytech.qgl.qab.actions.simple.ground.MoveTo;
-import fr.unice.polytech.qgl.qab.actions.simple.ground.Scout;
+import fr.unice.polytech.qgl.qab.actions.simple.ground.MoveTo; 
 import fr.unice.polytech.qgl.qab.util.enums.Direction;
 
 import java.util.ArrayList;
