21526d1e646f70bc1d0ca2f1b811f631a48519d4
Raquel LOPES DE OLIVEIRA <oliveira.raquel.lopes@gmail.com>
2016-03-21 22:46:38 +0100

    Not make transform is there no manufacturedResouce in the contract #QAB-115

--- src/main/java/fr/unice/polytech/qgl/qab/strategy/ground/GroundStrategy.java
+++ src/main/java/fr/unice/polytech/qgl/qab/strategy/ground/GroundStrategy.java
@@ -59,7 +59,7 @@ public class GroundStrategy implements IGroundStrategy {
      */
     private int contextAnalyzer(Context context) {
         // If all contracts are filled or there is with low quantity of budgets
-        if (context.contractsAreComplete() || context.getBudget() < getLimitBudget() || transformeFinished(context)){
+        if (context.contractsAreComplete() || context.getBudget() < getLimitBudget() || transformFinished(context)){
             return 1;
         }
 
@@ -68,13 +68,15 @@ public class GroundStrategy implements IGroundStrategy {
         * if all primary resources were not completed to make the manufactured, but will try to transform the maximum
         * before stops.
         * */
-        if (context.enoughToTransform() || context.getBudget() <= getLimitBudget() + BUDGET_TO_TRANSFORME){
-            return 2;
+        if(!(context.getResourcesToCreate() == null)){
+            if (context.enoughToTransform() || context.getBudget() <= getLimitBudget() + BUDGET_TO_TRANSFORME){
+                return 2;
+            }
         }
         return 0;
     }
 
-    private boolean transformeFinished(Context context) {
+    private boolean transformFinished(Context context) {
         return (context.getResourcesToCreate() != null && context.getResourcesToCreate().isEmpty());
     }
 }
